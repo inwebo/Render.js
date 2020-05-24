a35bcd73b32aa9b30ebe39da690ae627ea6c7476
inwebo <github@inwebo.net>
2020-05-24 10:55:38 +0200

    Add offscreen render demo

--- docs/assets/js/demo.js
+++ docs/assets/js/demo.js
@@ -1,3 +1,13 @@
-window.addEventListener("DOMContentLoaded", (event) => {
+import RenderOffScreen from "../../../src/RenderOffScreen";
 
+window.addEventListener("DOMContentLoaded", (event) => {
+    const canvas = document.getElementById('sprite');
+    const image  = new Image();
+
+    image.onload = (e) => {
+        const offscreenCanvas = new RenderOffScreen(canvas, image);
+        offscreenCanvas.draw();
+    };
+
+    image.src = 'assets/img/welcome.png';
 });
