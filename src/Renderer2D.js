b4830a381da7b28f9b764eb96a0ef57c5d33cdfa
inwebo <github@inwebo.net>
2020-05-25 20:01:30 +0200

    Add constructor

--- src/Renderer2D.js
+++ src/Renderer2D.js
@@ -6,10 +6,19 @@ export default class Renderer2D extends RendererAbstract {
      * @param {String|null} context 2d|webgl|webgl2|bitmaprenderer
      * @param {Object} contextAttributes
      * @see https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext
+     * @override
      * @return {CanvasRenderingContext2D}
      */
     getCtx(context= '2d', contextAttributes = {alpha: true}) {
         return this._canvas.getContext(context, contextAttributes);
+    }
+
+    /**
+     * @param {HTMLCanvasElement|HTMLElement|OffscreenCanvas} canvas
+     */
+    constructor(canvas) {
+        super();
+        this._canvas = canvas;
     }
 
     /**
